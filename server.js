e4c127d1bf5360fad6a4823b83e9a14ea7972847
Lok Wong <lywong@ucsd.edu>
2016-04-10 14:37:40 -0700

    added keys & part of passport.js

--- server.js
+++ server.js
@@ -44,14 +44,41 @@ app.use(parser.body.json());
 app.use(require('method-override')());
 app.use(session_middleware);
 /* TODO: Passport Middleware Here*/
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* TODO: Use Twitter Strategy for Passport here */
+passport.use(new strategy.Twitter({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: "/auth/twitter/callback"
+}, function(token, token_secret, profile, done) {
+    // What goes here? Refer to step 4.
+    models.User.findOne({ "twitterID": profile.id }, function(err, user) {
+    // (1) Check if there is an error. If so, return done(err);
+    if(!user) {
+        // (2) since the user is not found, create new user.
+        // Refer to Assignment 0 to how create a new instance of a model
+        return done(null, profile);
+    } else {
+        // (3) since the user is found, update user's information
+        process.nextTick(function() {
+            return done(null, profile);
+        });
+    }
+  });
+});
 
 /* TODO: Passport serialization here */
-
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 // Routes
 /* TODO: Routes for OAuth using Passport */
-// app.get("/", router.index.view);
+ app.get("/", router.index.view);
 // More routes here if needed
 
 // io.use(function(socket, next) {
